08bcd8e2c1f1975f3d2e6d3a5f83c4be22b9d963
Maksym Baldyniuk <frundzik1989@gmail.com>
2023-05-21 23:18:42 +0300

    fix task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -9,11 +9,9 @@ const galleryEl = document.querySelector('.gallery');
 const markupGallery = galleryItems
   .map(
     ({ original, preview, description }) =>
-      `<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
+      `<a class="gallery__link" href="${original}">
     <img class="gallery__image" src="${preview}" alt="${description}" />
-  </a>
-</li>`
+  </a>`
   )
   .join('');
 
