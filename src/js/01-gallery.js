feacdb8d5493cf1add1f024231e399c835543d2a
Maksym Baldyniuk <frundzik1989@gmail.com>
2023-05-21 21:15:50 +0300

    add task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const markupGallery = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`
+  )
+  .join('');
+
+galleryEl.insertAdjacentHTML('beforeend', markupGallery);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
